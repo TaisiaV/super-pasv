5380861ff29e0ee401eba1801862db8e63299201
taisiavolojanina <taisia.volojanina@gmail.com>
2022-10-18 17:47:21 -0400

    add environment variables support

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -8,7 +8,7 @@ describe('Auth', function () {
     it('Successful log in', function () {
         request
             .post('/auth')
-            .send({login: process.env.LOGIN, password: process.env.PASSWORD })
+            .send({login: process.env.LOGIN, password: process.env.PASSWORD})
             .end(function (err, res) {
                 expect(res.statusCode).to.eq(200)
                 expect(res.body.token).not.to.be.undefined
